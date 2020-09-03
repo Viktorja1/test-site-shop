b1931690321a8c0d7608642da9c350d5f5c7245e
Viktorja1 <30689417+Viktorja1@users.noreply.github.com>
2020-09-03 21:52:19 +0300

    Delete footer.php

--- old/parts/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-    <link rel="stylesheet" href="../css/footer.css">
-    <h2 class="contacts">Contacts</h2>
-    <h2>Our address: Lviv, Shevchenko str. 55</h2>
-    <h2>+38 066 12 34 567</h2>
-    <h2>honey_love@test.com</h2>
-    <h2 class="contacts">&copy All rights recived. <?php echo date('l jS \of F Y h:i:s A'); ?></h2>
-</div>
